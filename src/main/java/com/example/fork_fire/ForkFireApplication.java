fe96867c09e893c764b28e3c3f4e953e03554a6f
choi <choi@naver.com>
2025-05-07 17:08:47 +0900

    Initial

---
+++ src/main/java/com/example/fork_fire/ForkFireApplication.java
@@ -0,0 +1,13 @@
+package com.example.fork_fire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkFireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForkFireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fork_fire/ForkFireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fork_fire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForkFireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
